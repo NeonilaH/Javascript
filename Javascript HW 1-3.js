d514295b85ed8c775f5a85c8e4e43c25c93dbc5b
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-06-19 15:37:55 +0200

    Add files via upload

---
+++ Javascript HW 1-3.js
@@ -0,0 +1,22 @@
+// 2*: Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+let age_2 = 18
+let age_3 = 60
+const checkAge = function (age_1) {
+    if (typeof (age_1) === 'number') 
+    {
+        if (age_1 < age_2) { console.log("You don’t have access cause your age is " + age + ". It’s less than ") }
+        else if (age_1 >= age_2 && age_1 < age_3) {
+            console.log("Welcome !")
+        }
+        else if (age_1 > age_3) {
+            console.log("Keep calm and look Culture channel")
+        }
+        else {
+            console.log("Technical work")
+        }
+    } else {
+        console.log("Error")
+    }
+}
+checkAge(21)
+checkAge("yuvytv")
