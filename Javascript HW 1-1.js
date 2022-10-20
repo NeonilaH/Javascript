98409df2436a20dccde091cd118cf1dc14d97db4
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-10-20 10:51:19 +0200

    Update Javascript HW 1-1.js

--- Javascript HW 1-1.js
+++ Javascript HW 1-1.js
@@ -1,81 +1,81 @@
-// 1. Создать переменную “item_1”
-// 2. Присвоить переменной item_1 цифру 5
+// 1. Create a variable "item_1"
+// 2. Assign number 5 to the variable item_1 
 let item_1 = 5;
 
-// 3. Вывести в консоль item_1
-console.log(item_1);
+// 3. Print item_1 to the console
+console log(item_1);
 
-// 4. Создать переменную “item_2”
-// 5. Присвоить переменной item_2 цифру 3
+// 4. Create a variable "item_2"
+// 5. Assign number 3 to the variable item_2 
 let item_2 = 3;
 
-// 6. Вывести в консоль item_2
+// 6. Print item_2 to the console
 console.log(item_2);
 
-// 7. Создать переменную “item_3”
-// 8. Присвоить переменной item_3 сложение item_1 и item_2
+// 7. Create variable "item_3"
+// 8. Assign the addition of item_1 and item_2 to item_3
 let item_3 = item_1 + item_2;
 
-// 9. Вывести в консоль item_3
+// 9. Print item_3 to the console
 console.log(item_3);
 
-// 10. Создать переменную “item_4”
-// 11. Присвоить переменной item_4 строку “Yolochka”
+// 10. Create variable "item_4"
+// 11. Assign the string “Yolochka” to item_4
 let item_4 = "Yolochka";
 
-// 12. Вывести в консоль item_4
-console.log(item_4);
+// 12. Print item_4 to the console
+console log(item_4);
 
-// 13. Вывести в консоль сложение item_3 и item_4
+// 13. Print the addition of item_3 and item_4 to the console
 console.log(item_3 + item_4);
 
-// 14. Вывести в консоль умножение item_3 и item_4
+// 14. Print the multiplication of item_3 and item_4 to the console
 console.log(item_3 * item_4);
 
-// 15. Создать переменную “item_5”
-// 16. Присвоить переменной item_5 переменную item_3
+// 15. Create variable "item_5"
+// 16. Assign item_5 to item_3
 let item_5 = item_3;
 
-// 17. Создать переменную item_6
-// 18. Создать переменную item_6_type
-// 19. Присвоить переменной item_6 значение 15
-// 20. Присвоить переменной item_6_type тип переменной item_6
+// 17. Create variable item_6
+// 18. Create variable item_6_type
+// 19. Assign number 15 to the variable item_6
+// 20. Assign item_6_type variable the type of item_6 variable
 let item_6 = 15;
-let item_6_type = typeof (item_6);
+let item_6_type = typeof(item_6);
 
-// 21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——
+// 21. Print item_6 data type to console as —— “item_6 == ” item_6, “item_6_type == ” item_6_type ——
 console.log("item_6 ==" + item_6 + ", " + "item_6_type ==" + item_6_type);
 
-// 22. Создать переменную item_7 и в ней преобразовать item_6 в String
+// 22. Create variable item_7 and convert item_6 to String in it
 let item_7 = item_6.toString();
 
-// 23. Создать переменную item_7_type
-// 24. Присвоить переменной item_7_type тип переменной item_7
-let item_7_type = typeof (item_7);
+// 23. Create variable item_7_type
+// 24. Assign item_7_type variable the type of item_7 variable
+let item_7_type = typeof(item_7);
 
-// 25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——
-console.log("item_7 ==" + item_7 + ", " + "item_7_type ==" + item_7_type)
+// 25. Print item_7 data type to console as —— “item_7 == ” item_7, “item_7_type == ” item_7_type ——
+console.log("item_7 ==" + item_7 + ​​", " + "item_7_type ==" + item_7_type)
 
-// 26. Создать переменную “age_1” и присвоить ей значение 10
+// 26. Create a variable "age_1" and assign 10 to it
 let age_1 = 10;
 
-// 27. Создать переменную “age_2” и присвоить ей значение 18
+// 27. Create variable "age_2" and assign 18 to it
 let age_2 = 18;
 
-// 28. Создать переменную “age_3” и присвоить ей значение 60
+// 28. Create variable "age_3" and set assign 60 to it
 let age_3 = 60;
 
-// 29. Создать if в котором будите проверять значение переменной age_1
-// 30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “. It’s less then ”
-// 31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
-// 32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”
-// 33. Иначе выводите “Technical work”
+// 29. Create an if in which you will check the value of the variable age_1
+// 30. If age_1 < age_2, print “You don't have access cause your age is ” + age_1 + “ to the console. It's less then"
+// 31. If age_1 >= age_2 and age_1 < age_3, print “Welcome !” to the console
+// 32. If age_1 > age_3, output “Keep calm and look Culture channel” to the console
+// 33. Otherwise print "Technical work"
 if (age_1 < age_2) {
-    console.log("You don’t have access cause your age is " + age_1 + ". It’s less than ")
+    console.log("You don't have access cause your age is " + age_1 + ". It's less than ")
 } else if (age_1 >= age_2 && age_1 < age_3) {
-    console.log("Welcome !");
+    console.log("Welcome!");
 }
 else if (age_1 > age_3) {
-    console.log("Keep calm and look Culture channel");
+    console.log("Keep calm and look culture channel");
 }
-else { console.log("Technical work") }
+else { console. log("Technical work") }
