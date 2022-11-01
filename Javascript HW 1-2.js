b1988048c1e7e715f42b405e3bf4999b1eb9974d
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-01 15:48:44 +0100

    Update Javascript HW 1-2.js

--- Javascript HW 1-2.js
+++ Javascript HW 1-2.js
@@ -1,16 +1,11 @@
-// Задания с разным количеством звездочек:)
-// 1*:
-// Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
-// Пример: const checkAge = function(age) {
-// Ваши преобразования
-// Вывести в консоль результат работы функции с возрастами 17, 18, 61
+// 1*. Convert the code written in 26-33 into a function that takes `age` as input. Example: `const checkAge = function(age) { Your transformations }`. Print to the console the result of the function with ages 17, 18, 61.
 
 const checkAge = function (age_1) {
     let age_2 = 18;
     let age_3 = 60;
 
     if (age_1 < age_2) {
-        console.log("You don’t have access cause your age is " + age_1 + ". It’s less than ")
+        console.log("You don’t have access cause your age is " + age_1 + ". It’s less than " + age_2 + ".")
     }
     else if (age_1 >= age_2 && age_1 < age_3) {
         console.log("Welcome !")
