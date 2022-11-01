52b2762cf6a1b13286e91d7eae802d3f5042b412
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-01 16:54:33 +0100

    Update Javascript HW 1-3.js

--- Javascript HW 1-3.js
+++ Javascript HW 1-3.js
@@ -1,4 +1,4 @@
-// 2*. Convert [assignment 1*](https://github.com/NeonilaH/Javascript/blob/main/Javascript%20HW%201-2.js) so that the data type is checked first in the function. And if it is not a Number, an error is thrown.
+// 2*. Convert assignment 1* so that the data type is checked first in the function. And if it is not a Number, an error is thrown.
 
 let age_2 = 18
 let age_3 = 60
@@ -6,20 +6,21 @@ const checkAge = function (age_1) {
     if (typeof (age_1) === 'number') 
     {
         if (age_1 < age_2) { 
-            console.log("You don’t have access cause your age is " + age + ". It’s less than " + age_2 + ".") 
+            console.log("You don’t have access cause your age is " + age + ". It’s less than " + age_2 + ".");
         }
         else if (age_1 >= age_2 && age_1 < age_3) {
-            console.log("Welcome !")
+            console.log("Welcome !");
         }
         else if (age_1 > age_3) {
-            console.log("Keep calm and look Culture channel")
+            console.log("Keep calm and look Culture channel");
         }
         else {
-            console.log("Technical work")
+            console.log("Technical work");
         }
     } else {
         console.log("Error")
     }
 }
+
 checkAge(21)
 checkAge("yuvydtv")
