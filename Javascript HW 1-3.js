106e52df6304031e668c14f771695e9ce38d39c1
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-01 16:52:44 +0100

    Update Javascript HW 1-3.js

--- Javascript HW 1-3.js
+++ Javascript HW 1-3.js
@@ -1,11 +1,13 @@
-// 2*: Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+// 2*. Convert [assignment 1*](https://github.com/NeonilaH/Javascript/blob/main/Javascript%20HW%201-2.js) so that the data type is checked first in the function. And if it is not a Number, an error is thrown.
 
 let age_2 = 18
 let age_3 = 60
 const checkAge = function (age_1) {
     if (typeof (age_1) === 'number') 
     {
-        if (age_1 < age_2) { console.log("You don’t have access cause your age is " + age + ". It’s less than ") }
+        if (age_1 < age_2) { 
+            console.log("You don’t have access cause your age is " + age + ". It’s less than " + age_2 + ".") 
+        }
         else if (age_1 >= age_2 && age_1 < age_3) {
             console.log("Welcome !")
         }
