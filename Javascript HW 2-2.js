a5261abeb95079b76f189615228a5613f6bb90ae
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-02 13:57:11 +0100

    Update Javascript HW 2-2.js

--- Javascript HW 2-2.js
+++ Javascript HW 2-2.js
@@ -1,17 +1,16 @@
-// 2. Написать скрипт, который выведет 5 строк в консоль таким образом, чтобы в первой строчке выводилось :), во второй :):) и так далее
-// Пример в консоли:
+// 2. Write a script that will output 5 lines to the console in such a way that :) is displayed on the first line, :) :) on the second line, and so on. 
+// Console example:
 // :)
 // :):)
-// :):):)
-// :):):):)
+// :) :) :)
 // :):):):):)
+// :):):):):):)
 
 let b = ""
 for (let i = 1; i <= 5; i++)
     console.log(b += ':)')
 
-// 2*. Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода 
-//e.g. function printSmile(stroka, numberOfRows)
+// 2*. Convert task 2 into a function that takes a string as input, which will be output to the console (as in the smiley condition), as well as the number of lines to output. e.g. `function printSmile(line, numberOfRows)`.
 
 let x = ""
 function smile(stroka, numberOfRows) {
