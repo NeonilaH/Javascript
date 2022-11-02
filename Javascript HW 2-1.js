bff7c089924d0345dd14ad31a90a964aab7c5179
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-02 13:50:47 +0100

    Update Javascript HW 2-1.js

--- Javascript HW 2-1.js
+++ Javascript HW 2-1.js
@@ -1,9 +1,9 @@
-// 1. Написать скриптик, который сосчитает и выведет результат от возведения 2 в степень 10, начиная со степени 1
+// 1. Write a script that will calculate and display the result of raising 2 to the power of 10, starting from the 1st power.
 
 for (let i = 1; i <= 10; i++)
     console.log(Math.pow(2, i))
 
-// 1*. Преобразовать 1 задачу в функцию, принимающую на вход степень, в которую будет возводиться число 2
+// 1*. Convert 1 task into a function that takes as input the power to which the number 2 will be raised.
 
 const Exponenta = function (y) {
     console.log(2 ** y)
