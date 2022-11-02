be77cbc5ce0bfaf1da92beb72483b171d2af5d4d
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-02 13:46:29 +0100

    Update Javascript HW 1-5.js

--- Javascript HW 1-5/Javascript HW 1-5.js
+++ Javascript HW 1-5/Javascript HW 1-5.js
@@ -8,16 +8,16 @@ const checkAge = function (age_1) {
      if (!isNaN(age_1)) {
 
         if (age_1 < age_2) {
-            console.log("You don’t have access cause your age is " + age_1 + ". It’s less than " + age_2 + ".")
+            console.log("You don’t have access cause your age is " + age_1 + ". It’s less than " + age_2 + ".");
         }
         else if (age_1 >= age_2 && age_1 < age_3) {
-            console.log("Welcome !")
+            console.log("Welcome !");
         }
         else if
             (age_1 > age_3) {
-            console.log("Keep calm and look Culture channel")
+            console.log("Keep calm and look Culture channel");
         } else {
-            console.log("Technical work")
+            console.log("Technical work");
         }
     } else {
         console.log("Error")
