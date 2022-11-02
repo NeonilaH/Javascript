9a5243c97d98651c124111b07adaa726e2c7d963
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-02 13:30:16 +0100

    Update Javascript HW 1-4.js

--- Javascript HW 1-4.js
+++ Javascript HW 1-4.js
@@ -1,5 +1,4 @@
-// 3**:
-// Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+// 3**. Convert 2* so that the value '2' (the string containing ONLY NUMBER) is omitted when converted to number.
 
 let age_2 = 18
 let age_3 = 60
@@ -8,16 +7,16 @@ const checkAge = function (age_1) {
     if (!isNaN(age_1)) {
 
         if (age_1 < age_2) {
-            console.log("You don’t have access cause your age is " + age_1 + ". It’s less than ")
+            console.log("You don’t have access cause your age is " + age_1 + ". It’s less than " + age_2 + ".");
         }
         else if (age_1 >= age_2 && age_1 < age_3) {
-            console.log("Welcome !")
+            console.log("Welcome !");
         }
         else if
             (age_1 > age_3) {
-            console.log("Keep calm and look Culture channel")
+            console.log("Keep calm and look Culture channel");
         } else {
-            console.log("Technical work")
+            console.log("Technical work");
         }
     } else {
         console.log("Error")
