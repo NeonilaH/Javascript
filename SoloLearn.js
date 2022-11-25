0ac79167b3022a980a8574c1762ce48d6cd22214
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-25 11:17:02 +0100

    Update SoloLearn.js

--- SoloLearn.js
+++ SoloLearn.js
@@ -1,4 +1,4 @@
-var course = 1;
+let course = 1;
 if (course == 1) {
     document.write("<h1>HTML Tutorial</h1>");
 } else if (course == 2) {
@@ -8,7 +8,7 @@ if (course == 1) {
 }
 
 
-var score = 1;
+let score = 1;
 if (score >= 88) {
     console.log("excellent");
 }
@@ -19,7 +19,7 @@ else {
     console.log("fail");
 }
 
-var day = 2;
+let day = 2;
 switch (day) {
     case 1:
         document.write("Monday");
@@ -33,9 +33,6 @@ switch (day) {
     default:
         document.write("Another day");
 }
-
-function main() {
-    var themeNumber = parseInt(readLine(), 10)
 
 switch (themeNumber) {
     case 1:
@@ -58,10 +55,8 @@ switch (themeNumber) {
     console.log("Indigo")
     break;
 }
- 
-}
 
-var color ="yellow";
+let color ="yellow";
 switch(color) {
     case "blue": 
         document.write("This is blue.");
@@ -77,86 +72,4 @@ switch(color) {
         break;
     default: 
         document.write("Color not found.");
-}
-
-// a for loop that prints numbers 1 through 5:
-
-for (i=1; i<=5; i++) {
-    document.write(i + "<br />");
-}
-
-for (i=1; i<=5; i++) {
-    document.write(i);
-}
-
-var i = 1;
-for (; i<=5; i++) {
-    document.write(i + "<br />");
-}
-
-for (i=1, text=""; i<=5; i++) {
-    text = i;
-    document.write(i + "<br />");
-}
-
-// Fill in the blanks to print EVEN values from 0 to 20 using a for loop:
-var x = 0;
-
-for (; x <= 
-20
-; x += 
-2
-) {
-
-   document.write(x); 
-
-}
-
-var i=0;
-while (i<=10) {
-    document.write(i + "<br />");
-    i++;
-}
-
-//Write a program-timer, that will take the count of seconds as input and output to the console all the seconds until timer stops.
-function main() {
-    var seconds = parseInt(readLine(), 10)
-    // Your code here
-    while (seconds >= 0) {
-        console.log (seconds);
-        seconds--
-    }
-}
-
-// do..while loop will execute the code block once, before checking if the condition is true, and then it will repeat the loop as long as the condition is true.
-var i=20;
-do {  
-    document.write(i + "<br />");
-    i++;  
-}
-while (i<=25); 
-
-// break - we use it to "jump out" of a loop and continue executing the code after the loop.
-// continue - breaks only one iteration in the loop, and continues with the next iteration.
-for (i = 0; i <= 10; i++) {
-    if (i == 5) {
-        break; 
-    }
-    document.write(i + "<br />");
-}
-
-for (i = 0; i <= 10; i++) {
-    if (i == 5) {
-        continue;
-    }
-    document.write(i + "<br />");
-}
-
-// Many tall buildings, including hotels, skip the number 13 when numbering floors -- often going from floor 12 to floor 14. It is thought that the number 13 is unlucky. Write a program that will number 15 rooms starting from 1, skipping the number 13. Output to the console each room number in separate line.
-var countOfRooms = 15;
-for (i=1; i <= 16; i++) {
-if (i==13) {
-    continue;
-    }
-    console.log(i)
 }
