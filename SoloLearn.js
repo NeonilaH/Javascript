ed1effdbfc4f732b833b3a1abbf53e67f73cb016
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-26 14:42:13 +0100

    Update SoloLearn.js

--- SoloLearn.js
+++ SoloLearn.js
@@ -198,3 +198,32 @@ if (result == true) {
 else {
     alert("Thanks for staying with us");
 }
+
+//Creating Your Own Objects
+function person(name, age, color) {
+    this.name = name;
+    this.age = age;
+    this.favColor = color;
+}
+var p1 = new person("John", 42, "green");
+var p2 = new person("Amy", 21, "red");
+document.write(p1.age); 
+document.write(p2.name);
+
+/* Bob was hired as an airport information officer and needs to generate status messages for each flight․ Let’s help him!
+Complete the given program by fixing the constructor, making a flight object, and assign it to given variable to correctly execute the corresponding message.
+Flight ID and the flight status(landed, on time, delayed, etc.) are taken as input.*/
+function main() {
+    //take flight number and its status
+    var flightNumber = readLine();
+    var flightStatus = readLine();
+        var flight1 = new Flight(flightNumber, flightStatus);
+    //assign a flight object to flight1 variable
+    //output
+    console.log('The flight ' + flight1.number + ' is ' + flight1.status)
+    }
+function Flight(flightNumber, status) {
+    //fix the constructor
+    this.number = flightNumber;
+    this.status = status;
+};
