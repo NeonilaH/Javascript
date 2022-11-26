64f2eba788da8fae4b6d2345d2113024a03d9642
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-26 14:23:14 +0100

    Update SoloLearn.js

--- SoloLearn.js
+++ SoloLearn.js
@@ -165,3 +165,36 @@ function main() {
 function setReminder(event) {
     console.log("You set a reminder about " + event)
 };
+
+/*You are given a program that takes Team 1 and Team 2 football teams goals as inputs accordingly.
+Complete the function to take Team 1 and Team 2 goals as arguments and output the final result of the match:
+- "Team 1 won", if Team 1's score is higher than Team 2's score
+- "Team 2 won", if Team 2's score is higher than Team 1's score
+- "Draw", if the scores are equal*/
+let goalsTeam1 = 0
+let goalsTeam2 = 1
+function finalResult(goalsTeam1, goalsTeam2) {
+    if (goalsTeam1 > goalsTeam2) {
+        console.log("Team 1 won")
+    }
+    else if (goalsTeam1 < goalsTeam2) {
+        console.log("Team 2 won")
+    }
+    else {
+        console.log("Draw")
+    }
+};
+
+// Alert, Prompt, Confirm
+alert("Do you really want to leave this page?");
+
+var user = prompt("Please enter your name");
+alert(user);
+
+var result = confirm("Do you really want to leave this page?");
+if (result == true) {
+    alert("Thanks for visiting");
+}
+else {
+    alert("Thanks for staying with us");
+}
