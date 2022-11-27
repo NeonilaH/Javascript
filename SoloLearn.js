cd3e3829e6bf5ae17d7ede8461a0b364a40cfcdd
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-27 18:13:10 +0100

    Update SoloLearn.js

--- SoloLearn.js
+++ SoloLearn.js
@@ -354,3 +354,20 @@ function printTime() {
     document.body.innerHTML = hours+":"+mins+":"+secs;
 }
 setInterval(printTime, 1000);
+
+// The program you are given takes year, month and day as input. Create a function that takes them as arguments and returns the corresponding day of the week.
+function main() {
+    var year = parseInt(readLine(), 10);
+    var month = parseInt(readLine(), 10);
+    var day = parseInt(readLine(), 10);
+     
+    console.log(getWeekDay(year, month, day)); 
+ }
+ 
+ function getWeekDay(year, month, day) {
+    var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var d = new Date(year, month, day);
+     //complete the function
+    var index = d.getDay()
+    return names[index]
+ }
