5670273cea2c153face9a0b2160fdfe672448077
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-27 18:38:30 +0100

    Update SoloLearn.js

--- SoloLearn.js
+++ SoloLearn.js
@@ -371,3 +371,12 @@ function main() {
     var index = d.getDay()
     return names[index]
  }
+
+// You are working on a Store Manager program, which stores the prices in an array. You need to add functionality to increase the prices by the given amount. The increase variable is taken from user input. You need to increase all the prices in the given array by that amount and output to the console the resulting array.
+function main() {
+    var increase = parseInt(readLine(), 10);
+    var prices = [98.99, 15.2, 20, 1026];
+    //your code goes here
+    var newArray = prices.map(el => el + increase)
+    console.log(newArray)
+}
