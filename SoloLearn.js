d4f5d28d80c70e65df9d5752b9dc7b01fd0cb905
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-29 12:54:55 +0100

    Update SoloLearn.js

--- SoloLearn.js
+++ SoloLearn.js
@@ -569,3 +569,14 @@ let a = 8;
 let b = 34;
 let msg = `The sum is ${a+b}`;
 console.log(msg);
+
+// Students need to score at least 70 points to pass an exam. The given program declares an array with results. Write a program to count and output to the console the number of students who pass the exam.
+let scores = [68,95,54,84,77,75,63,74,69,80,71,63]
+var count = 0
+for (let x of scores) {
+    if (x >= 70) {
+        count++
+    }
+    
+}
+console.log(count)
