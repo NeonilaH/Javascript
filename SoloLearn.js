d1e73197597bab7f421f7b717d37a35746dedcd8
NeonilaH <106426661+NeonilaH@users.noreply.github.com>
2022-11-29 11:01:31 +0100

    Update SoloLearn.js

--- SoloLearn.js
+++ SoloLearn.js
@@ -535,7 +535,24 @@ function prev() {
     }
     slider.src = images[num];
 }
-
-
+// let's create a form with two inputs and one button. The text in both fields should be the same and not blank to pass the validation.
+//html
+<form onsubmit="return validate()" method="post">
+    Number: <input type="text" name="num1" id="num1" /><br />
+    Repeat: <input type="text" name="num2" id="num2" /><br />
+    <input type="submit" value="Submit" />
+</form>
+//js
+function validate() {
+    var n1 = document.getElementById('num1');
+    var n2 = document.getElementById('num2');
+    if(n1.value != '' && n2.value != '') {
+        if(n1.value == n2.value) {
+            return true;
+        }
+    }
+    alert("The values should be equal and not blank");
+    return false;
+}
 
 
